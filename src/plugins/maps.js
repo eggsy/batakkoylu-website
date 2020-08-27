dc95a576addcb0f8b3922c5d5f834dc7357c9d62
Abdulbaki Dursun <eggsydev@outlook.com>
2020-08-27 03:38:08 +0300

    :wrench: netlify adds $?

--- src/plugins/maps.js
+++ src/plugins/maps.js
@@ -5,7 +5,7 @@ Vue.use(VueGoogleMaps, {
   load: {
     region: "TR",
     language: "tr",
-    key: process.env.GOOGLE_API_KEY,
+    key: process.env.GOOGLE_API_KEY || process.env.$GOOGLE_API_KEY,
     libraries: "places",
   },
 });
